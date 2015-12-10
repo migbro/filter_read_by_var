798fcdb1bf7626c0d4a9dcf3602b4a4a78ce9b29
migbro <miguel.a.brown@gmail.com>
2015-12-10 16:57:26 -0600

    Ready to test.  Needs wrapper script if vcfs split.

--- filter_read_by_var.py
+++ filter_read_by_var.py
@@ -1,24 +1,54 @@
 #!/usr/bin/env python
 '''
-Usage: ./filter_read_by_var.py <vcf> <bam>
+Usage: ./filter_read_by_var.py <vcf> <bam1> <bam2> <out>
 
 Arguments:
 <vcf> vcf file with variant to search for
-<bam> bam file to extract reads from
+<bam1> bam file to extract reads from
+<bam2> bam file to check reads in
+<out>  out file prefix
 
 Options:
 -h
 
 '''
-import sys
+
 import pysam
-import pdb
 from docopt import docopt
+
 args = docopt(__doc__)
 
-bam_file = pysam.AlignmentFile(args['<bam>'], 'rb')
+bam_file = pysam.AlignmentFile(args['<bam1>'], 'rb')
 vcf_file = pysam.VariantFile(args['<vcf>'], 'r')
-
+i = 0
+reads = []
+var_objs = []
+var_flag = {}
 for variant in vcf_file.fetch():
+    var_objs.append(variant)
+    var_flag[i] = 0
+    var = var.alts[2]
     for read in bam_file.fetch(variant.chrom, variant.pos):
-        pdb.set_trace()
+        pos = variant.pos - read.pos - 1
+        if read.read.query_alignment_sequence[pos] == var:
+            reads[read.qname] = []
+            reads[read.qname]['pos'] = pos
+            reads[read.qname]['var'] = var
+            reads[read.qname]['v_idx'] = i
+            # pdb.set_trace()
+    i += 1
+bam_file.close()
+vcf_file.close()
+
+mmu_bam = pysam.AlignmentFile(args['<bam2>'], 'rb')
+
+for read in mmu_bam:
+    if read.qname in reads and read.seq[reads[read.qname]['pos']] == reads[read.qname]['var']:
+        var_flag[reads[read.qname]['v_idx']] += 1
+mmu_bam.close()
+out = open(args['<out>'], 'w')
+for index in var_flag:
+    if var_flag[index] > 0:
+        out.write('\t'.join(
+                (var_objs[index].chrom, str(var_objs[index].pos), var_objs[index].ref, var_objs[index].alts[2])) + str(
+                var_flag[index]) + '\n')
