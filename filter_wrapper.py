49dc2c4ded9f246ab6a1a9192a383635c0b61315
migbro <miguel.a.brown@gmail.com>
2015-12-11 16:04:49 -0600

    Added better log output to wrapper

--- filter_wrapper.py
+++ filter_wrapper.py
@@ -30,6 +30,8 @@ for vcf in vcf_list:
     parts = vcf.split('\.')
     out = parts[0] + '.hits.txt'
     cmd = ' '.join((filter_py, vcf, bam1, bam2, out))
+    log_file = parts[0] + '.log'
+    cmd += ' 2> ' + log_file
     cmd_list.append(cmd)
 vcf_list.close()
 job_manager(cmd_list, th)
