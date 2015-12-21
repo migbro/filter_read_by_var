71ab33a1aadf41f2b99bc37c447adcf92181bc79
migbro <miguel.a.brown@gmail.com>
2015-12-21 14:59:16 -0600

    Working through indexed bam

--- filter_read_by_var.py
+++ filter_read_by_var.py
@@ -79,50 +79,53 @@ for read in mmu_bam:
 out.close()
 sys.stderr.write('Creating filtered reads bam index\n')
 pysam.index(mmu_filtered)
-pdb.set_trace()
+# pdb.set_trace()
 mmu_bam.close()
 mmu_subset_bam = pysam.AlignmentFile(mmu_filtered, 'rb')
 sys.stderr.write('Indexing filtered reads\n')
 mmu_subset_bai = pysam.IndexedReads(mmu_subset_bam, 1)
-mmu_subset_bai.build()
-#     try:
-#         # make same adjustment above for deletion
-#         cur_pos = reads[read.qname]['pos']
-#         m = re.findall('(\d+\w)', read.cigarstring)
-#         for cig in m:
-#             if cig[-1] == 'D':
-#                 cur_pos -= int(cig[:-1])
-#         if read.qname in reads and read.seq[cur_pos] == reads[read.qname]['var']:
-#             index = reads[read.qname]['v_idx']
-#             if index not in var_flag:
-#                 var_flag[index] = {}
-#                 # store corresponding mouse info
-#                 var_flag[index]['chr'] = mmu_bam.getrname(read.tid)
-#                 var_flag[index]['pos'] = read.pos + cur_pos
-#                 var_flag[index]['r1'] = 0
-#                 var_flag[index]['r2'] = 0
-#                 var_flag[index]['paired'] = 0
-#                 var_flag[index]['var'] = 0
-#             var_flag[index]['var'] += 1
-#             if read.is_read1:
-#                 var_flag[index]['r1'] += 1
-#             else:
-#                 var_flag[index]['r2'] += 1
-#             if read.is_paired:
-#                 var_flag[index]['paired'] += 1
-#
-#
-#     except:
-#         # sys.stderr.write('Error at read ' + str(j) + ' skipping!\n')
-#         err_ct += 1
-#         continue
+mmu_subset_bai.build
+for read in reads:
+    try:
+        # make same adjustment above for deletion
+        qset = mmu_subset_bai.find(read)
+        pdb.set_trace()
+        cur_pos = reads[read.qname]['pos']
+        m = re.findall('(\d+\w)', read.cigarstring)
+        for cig in m:
+            if cig[-1] == 'D':
+                cur_pos -= int(cig[:-1])
+        if read.qname in reads and read.seq[cur_pos] == reads[read.qname]['var']:
+            index = reads[read.qname]['v_idx']
+            if index not in var_flag:
+                var_flag[index] = {}
+                # store corresponding mouse info
+                var_flag[index]['chr'] = mmu_bam.getrname(read.tid)
+                var_flag[index]['pos'] = read.pos + cur_pos
+                var_flag[index]['r1'] = 0
+                var_flag[index]['r2'] = 0
+                var_flag[index]['paired'] = 0
+                var_flag[index]['var'] = 0
+            var_flag[index]['var'] += 1
+            if read.is_read1:
+                var_flag[index]['r1'] += 1
+            else:
+                var_flag[index]['r2'] += 1
+            if read.is_paired:
+                var_flag[index]['paired'] += 1
 
-# sys.stderr.write(str(err_ct) + ' mouse reads skipped due to missing cigar or invalid positioning\n')
-# out = open(args['<out>'], 'w')
-# for index in var_flag:
-#     out.write('\t'.join(
-#             (var_objs[index].chrom, str(var_objs[index].pos), var_objs[index].ref,
-#              var_objs[index].alts[0])) + '\t' + '\t'.join((str(var_flag[index]['var']), var_flag[index]['chr'],
-#                                                            str(var_flag[index]['pos']), str(var_flag[index]['r1']),
-#                                                            str(var_flag[index]['r2']),
-#                                                            str(var_flag[index]['paired']))) + '\n')
+
+    except:
+        # sys.stderr.write('Error at read ' + str(j) + ' skipping!\n')
+        err_ct += 1
+        continue
+
+sys.stderr.write(str(err_ct) + ' mouse reads skipped due to missing cigar or invalid positioning\n')
+out = open(args['<out>'], 'w')
+for index in var_flag:
+    out.write('\t'.join(
+            (var_objs[index].chrom, str(var_objs[index].pos), var_objs[index].ref,
+             var_objs[index].alts[0])) + '\t' + '\t'.join((str(var_flag[index]['var']), var_flag[index]['chr'],
+                                                           str(var_flag[index]['pos']), str(var_flag[index]['r1']),
+                                                           str(var_flag[index]['r2']),
+                                                           str(var_flag[index]['paired']))) + '\n')
