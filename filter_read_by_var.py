51db2f1732245640f5a4f35e04fda326740b6f43
migbro <miguel.a.brown@gmail.com>
2015-12-22 17:05:48 -0600

    Inserted logic to mimic mutect read acceptance parameters to drastically reduce false positives

--- filter_read_by_var.py
+++ filter_read_by_var.py
@@ -33,6 +33,72 @@ var_flag = {}
 j = 1
 mod = 100
 err_ct = 0
+
+def mutect_check(read_obj, align_obj, skip_dict):
+    frac = 0.3
+    if read_obj.qname in skip_dict:
+        return skip_dict, 0
+    try:
+        slen = 0
+        clip = 0
+        ins = []
+        ins_size = []
+        clip_pos = []
+        clip_size = []
+        m = re.findall('(\d+\w)', read_obj.cigarstring)
+        for cig in m:
+            slen += int(cig[:-1])
+            # need to track number of bases clipped AND move backwards of clipped
+            if cig[-1] == 'S' or cig[-1] == 'H':
+                clip += int(cig[:-1])
+                clip_pos.append(slen)
+                clip_size.append(int(cig[:-1]))
+
+            # need to move position later if there's an insertion forward
+            if cig[-1] == 'I':
+                ins.append(slen)
+                ins_size.append(int(cig[:-1]))
+        if not read_obj.is_proper_pair:
+            sys.stderr.write('Not a proper pair!\n')
+
+        if float(clip)/slen < frac and read_obj.mapping_quality > 0:
+            if read_obj.has_tag('MD'):
+                mm = re.findall('(\d+\w)', read_obj.get_tag('MD'))
+                rqual = 0
+                cur = 0
+                for md in mm:
+                    pos = cur + int(md[:-1])
+                    for i in xrange(0, len(clip_pos), 1):
+                        if pos > clip_pos[i]:
+                            pos -= clip_size[i]
+                            break
+                    for i in xrange(0, len(ins), 1):
+                        if pos > ins[i]:
+                            pos += ins_size[i]
+                            break
+                    rqual += read_obj.query_alignment_qualities[pos]
+                    cur = pos + 1
+                if rqual > 100:
+                    #pdb.set_trace()
+                    sys.stderr.write('Sum quality of mismatches too high, skipping\n')
+                    return skip_dict, 0
+            if abs(read_obj.tlen) < 202:
+                test = align_obj.mate(read_obj)
+                if test.mapping_quality > read_obj.mapping_quality:
+                    return skip_dict, 0
+                else:
+                    skip_dict[test.qname] = 1
+                    return skip_dict, 1
+
+            else:
+                skip_dict[read_obj.qname] = 1
+                return skip_dict, 1
+    except:
+        # pdb.set_trace()
+        sys.stderr.write('Error!\n')
+        return skip_dict, 0
+
+to_skip = {}
 for variant in vcf_file.fetch():
     if j % mod == 0:
         sys.stderr.write('Processing variant ' + str(j) + ' in vcf file\n')
@@ -43,7 +109,7 @@ for variant in vcf_file.fetch():
     var = variant.alts[0]
     for read in bam_file.fetch(variant.chrom, variant.pos, (variant.pos + 1)):
         pos = variant.pos - read.pos - 1
-        if pos >= 0:
+        if pos >= 0 and read.is_proper_pair:
             try:
                 m = re.findall('(\d+\w)', read.cigarstring)
                 for cig in m:
@@ -53,13 +119,17 @@ for variant in vcf_file.fetch():
             except:
                 err_ct += 1
                 continue
+            # first check that read matches variant, then if it'd pass mutect filters
             if read.query_alignment_sequence[pos] == var:
-                reads[read.qname] = {}
-                reads[read.qname]['pos'] = pos
-                read_out.write('\t'.join((read.qname, bam_file.getrname(read.tid), str(variant.pos))) + '\n')
-                reads[read.qname]['var'] = var
-                reads[read.qname]['v_idx'] = i
-                # pdb.set_trace()
+                flag = 0
+                (to_skip, flag) = mutect_check(read, bam_file, to_skip)
+                if flag == 1:
+                    reads[read.qname] = {}
+                    reads[read.qname]['pos'] = pos
+                    read_out.write('\t'.join((read.qname, bam_file.getrname(read.tid), str(variant.pos))) + '\n')
+                    reads[read.qname]['var'] = var
+                    reads[read.qname]['v_idx'] = i
+                    # pdb.set_trace()
     i += 1
 sys.stderr.write(str(err_ct) + ' reads skipping in bam1 due to invalid cigar or positioning\n')
 bam_file.close()
@@ -103,7 +173,7 @@ for read in mmu_subset_bam.fetch():
             if index not in var_flag:
                 var_flag[index] = {}
                 # store corresponding mouse info
-                var_flag[index]['chr'] = mmu_bam.getrname(read.tid)
+                var_flag[index]['chr'] = mmu_subset_bam.getrname(read.tid)
                 var_flag[index]['pos'] = read.pos + cur_pos
                 var_flag[index]['r1'] = 0
                 var_flag[index]['r2'] = 0
